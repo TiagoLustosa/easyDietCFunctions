20c6a0e07d8c40180550e85ad2ff6925d6b97b86
TiagoLustosa <tiagolustosasantos@hotmail.com>
2023-05-02 11:15:40 -0300

    in prod

--- functions/index.js
+++ functions/index.js
@@ -6,35 +6,13 @@ exports.diet = functions.database.ref('/user/{id}').onCreate((snapshot, context)
     const macros = calculateTotalMacros(userData);
 
 
-    // if (context.params.id == 'QrWi0NJpQYP31S1B4mVbxFfMJ1L2' || context.params.id == 'uwzRdt1fOZYoXaFyNUZRXYpeTQ82') {
+
     totalProteinInAllMeals = macros.totalProtein
     totalLipidInAllMeals = macros.totalLipid
     totalCaloriesInAllMeals = bmr
 
     const diet = calculateDiet(userData);
     return snapshot.ref.parent.child(`${context.params.id}`).update({ bmr: bmr, diet: diet, macros: macros });
-    // } else {
-    //     totalProteinInMeal = macros.totalProtein
-    //     totalLipidInMeal = macros.totalLipid
-    //     totalCaloriesInMeal = bmr
-
-    //     totalProteinFirstMeal = totalProteinInMeal * 0.2
-    //     totalLipidFirstMeal = totalLipidInMeal * 0.2
-    //     totalCaloriesFirstMeal = totalCaloriesInMeal * 0.2
-
-    //     totalProteinSecondMeal = totalProteinInMeal * 0.3
-    //     totalLipidSecondMeal = totalLipidInMeal * 0.3
-    //     totalCaloriesInSecondMeal = totalCaloriesInMeal * 0.3
-
-    //     totalProteinThirdMeal = totalProteinInMeal * 0.2
-    //     totalLipidThirdMeal = totalLipidInMeal * 0.2
-    //     totalCaloriesInThirdMeal = totalCaloriesInMeal * 0.2
-
-    //     const diet = calculateDietFourMeals(userData);
-    //     return snapshot.ref.parent.child(`${context.params.id}`).update({ bmr: bmr, diet: diet, macros: macros });
-    // }
-
-
 });
 
 exports.dietUpdate = functions.database.ref('/user/{id}').onUpdate((change, context) => {
@@ -44,34 +22,12 @@ exports.dietUpdate = functions.database.ref('/user/{id}').onUpdate((change, cont
     const userData = change.after.val();
     const bmr = calculateBasalMetabolicRate(userData);
     const macros = calculateTotalMacros(userData);
-    // if (context.params.id == 'QrWi0NJpQYP31S1B4mVbxFfMJ1L2' || context.params.id == 'uwzRdt1fOZYoXaFyNUZRXYpeTQ82') {
+
     totalProteinInAllMeals = macros.totalProtein
     totalLipidInAllMeals = macros.totalLipid
     totalCaloriesInAllMeals = bmr
     const diet = calculateDiet(userData);
     return change.after.ref.parent.child(`${context.params.id}`).update({ bmr: bmr, diet: diet, macros: macros });
-    // } else {
-    //     totalProteinInMeal = macros.totalProtein
-    //     totalLipidInMeal = macros.totalLipid
-    //     totalCaloriesInMeal = bmr
-
-    //     totalProteinFirstMeal = totalProteinInMeal * 0.2
-    //     totalLipidFirstMeal = totalLipidInMeal * 0.2
-    //     totalCaloriesFirstMeal = totalCaloriesInMeal * 0.2
-
-    //     totalProteinSecondMeal = totalProteinInMeal * 0.3
-    //     totalLipidSecondMeal = totalLipidInMeal * 0.3
-    //     totalCaloriesInSecondMeal = totalCaloriesInMeal * 0.3
-
-    //     totalProteinThirdMeal = totalProteinInMeal * 0.2
-    //     totalLipidThirdMeal = totalLipidInMeal * 0.2
-    //     totalCaloriesInThirdMeal = totalCaloriesInMeal * 0.2
-
-    //     const diet = calculateDietFourMeals(userData);
-    //     return change.after.ref.parent.child(`${context.params.id}`).update({ bmr: bmr, diet: diet, macros: macros });
-    // }
-
-
 });
 
 function calculateBasalMetabolicRate(userData) {
@@ -112,94 +68,6 @@ function calculateBasalMetabolicRate(userData) {
             - (4.676 * userData.age)) * activityLevelFactor + dietObjective;
     }
 }
-
-// const calculateDiet = (foodList, totalProtein, totalLipid, bmrResult) => {
-//     let totalProteinGrams = 0, proteinResult = 0;
-//     let totalLipidGrams = 0, lipidResult = 0;
-//     let totalCarboGrams = 0, carboResult = 0;
-//     let totalKcal = 0.0;
-//     const protein = foodList.find((e) => e.highProtein == true);
-//     const lipid = foodList.find((e) => e.highLipid == true);
-//     const carbo = foodList.find((e) => e.highCarb == true);
-
-//     const proteinPerGram = protein.protein / 100;
-//     const proteinKcalPerGram = protein.energyInKcal / 100;
-//     const proteinLipidPerGram = protein.lipid / 100;
-//     const proteinCarboPerGram = protein.carbohydrate / 100;
-
-//     const lipidPerGram = lipid.lipid / 100;
-//     const lipidKcalPerGram = lipid.energyInKcal / 100;
-//     const lipidProteinPerGram = lipid.protein / 100;
-//     const lipidCarboPerGram = lipid.carbohydrate / 100;
-
-//     const carboPerGram = carbo.carbohydrate / 100;
-//     const carboKcalPerGram = carbo.energyInKcal / 100;
-//     const carboLipidPerGram = carbo.lipid / 100;
-//     const carboProteinPerGram = carbo.protein / 100;
-//     while (totalKcal < bmrResult) {
-//         while (proteinResult < totalProtein) {
-//             proteinResult += proteinPerGram;
-//             totalKcal += proteinKcalPerGram;
-//             carboResult += proteinCarboPerGram;
-//             lipidResult += proteinLipidPerGram;
-//             totalProteinGrams++;
-//         }
-//         if (lipidResult < totalLipid) {
-//             proteinResult += lipidProteinPerGram;
-//             totalKcal += lipidKcalPerGram;
-//             carboResult += lipidCarboPerGram;
-//             lipidResult += lipidPerGram;
-//             totalLipidGrams++;
-
-//         }
-//         if ((totalKcal >= bmrResult)) {
-
-//             while ((proteinResult < totalProtein) && (lipidResult > totalLipid)) {
-//                 //salmao quebra pq tem "bastante proteina e bastante gordura"
-//                 proteinResult -= lipidProteinPerGram;
-//                 totalKcal -= lipidKcalPerGram;
-//                 carboResult -= lipidCarboPerGram;
-//                 lipidResult -= lipidPerGram;
-//                 totalLipidGrams--;
-
-//                 proteinResult += proteinPerGram;
-//                 totalKcal += proteinKcalPerGram;
-//                 carboResult += proteinCarboPerGram;
-//                 lipidResult += proteinLipidPerGram;
-//                 totalProteinGrams++;
-//             }
-
-//         }
-//         if (proteinResult > (totalProtein + (totalProtein * 0.03))) {
-//             while (proteinResult > (totalProtein + (totalProtein * 0.03))) {
-//                 proteinResult -= proteinPerGram;
-//                 totalKcal -= proteinKcalPerGram;
-//                 carboResult -= proteinCarboPerGram;
-//                 lipidResult -= proteinLipidPerGram;
-//                 totalProteinGrams--;
-//             }
-//         }
-
-//         proteinResult += carboProteinPerGram;
-//         totalKcal += carboKcalPerGram;
-//         carboResult += carboPerGram;
-//         lipidResult += carboLipidPerGram;
-//         totalCarboGrams++;
-
-//     }
-
-//     const diet = {
-//         totalKcal: totalKcal,
-//         protein: totalProteinGrams,
-//         lipid: totalLipidGrams,
-//         carbohydrate: totalCarboGrams,
-//         foods: foodList,
-//         totalProteinInDiet: proteinResult,
-//         totalLipidInDiet: lipidResult,
-//         totalCarboInDiet: carboResult,
-//     }
-//     return diet;
-// }
 
 const calculateTotalMacros = (userData) => {
     const totalProtein = userData.weight * userData.proteinPerKilogramOfBodyWeight;
